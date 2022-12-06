be4591b33d24792284f17f734a31f356a837274d
Jitty-04 <jittysjohn1997@gmail.com>
2022-12-06 10:32:44 +0530

    addread

---
+++ src/main/java/com/nest/employee_backend/model/Employee.java
@@ -0,0 +1,109 @@
+package com.nest.employee_backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String empcode;
+    private String empname;
+    private String designation;
+    private int salary;
+    private String companyname;
+    private String mbno;
+    private String username;
+    private String password;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String empcode, String empname, String designation, int salary, String companyname, String mbno, String username, String password) {
+        this.id = id;
+        this.empcode = empcode;
+        this.empname = empname;
+        this.designation = designation;
+        this.salary = salary;
+        this.companyname = companyname;
+        this.mbno = mbno;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmpcode() {
+        return empcode;
+    }
+
+    public void setEmpcode(String empcode) {
+        this.empcode = empcode;
+    }
+
+    public String getEmpname() {
+        return empname;
+    }
+
+    public void setEmpname(String empname) {
+        this.empname = empname;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getCompanyname() {
+        return companyname;
+    }
+
+    public void setCompanyname(String companyname) {
+        this.companyname = companyname;
+    }
+
+    public String getMbno() {
+        return mbno;
+    }
+
+    public void setMbno(String mbno) {
+        this.mbno = mbno;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
